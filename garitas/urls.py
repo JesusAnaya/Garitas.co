749a53306effe003bb72f56113f029ed20981a12
JesusAnaya <jesus.anaya.dev@gmail.com>
2013-08-02 17:42:50 -0700

    add gunicorn and new url module

--- garitas/urls.py
+++ garitas/urls.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
-from django.conf.urls import patterns, url
+try:
+    from django.conf.urls import patterns, url
+except ImportError:
+    from django.conf.urls.defaults import patterns, url
+
 from garitas import sitemap
 
 urlpatterns = patterns('',
--- garitas_project/settings.py
+++ garitas_project/settings.py
@@ -120,14 +120,12 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     'django.contrib.sitemaps',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
     'api',
     'garitas',
-    'jstemplate'
+    'jstemplate',
+    'gunicorn',
 )
 
 # A sample logging configuration. The only tangible logging
